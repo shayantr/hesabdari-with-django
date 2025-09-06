3ce92bf07e4255bd74d635100ca82b2e19e6e333
shayantr <61402605+shayantr@users.noreply.github.com>
2025-09-06 17:41:35 +0330

    fixed ui in balance_list.html

--- src/hesabdari/apps/account_base/views.py
+++ src/hesabdari/apps/account_base/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction, DatabaseError
-from django.db.models import Q, Prefetch, Max, Sum, Case, IntegerField, When
+from django.db.models import Q, Prefetch, Max, Sum, Case, IntegerField, When, F
 from django.db.transaction import commit
 from django.http import HttpResponseRedirect, JsonResponse, HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -190,6 +190,39 @@ class GetFormFragmentView(generic.View):
 
 def account_report(request):
     return render(request, 'account_base/accounts-report.html')
+# class AccountsView(generic.View):
+#     def get(self, request):
+#         def serialize_node(node, net_total):
+#             return {
+#                 'id': str(node.id),
+#                 'text': node.name,
+#                 'parent': str(node.parent.id) if node.parent else '#',
+#                 'li_attr': {'data-net-total': net_total},
+#             }
+#
+#         accounts = AccountsClass.objects.filter(user=request.user).prefetch_related(
+#             Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user)))
+#         data= []
+#         for account in accounts:
+#             descendants = account.get_descendants(include_self=True)
+#             total_debtor = 0
+#             total_creditor = 0
+#             for descendant in descendants:
+#                 for bs in descendant.balance_sheets.all():
+#                     if bs.amount:
+#                         if bs.transaction_type == 'debt':
+#                             total_debtor += bs.amount
+#                         elif bs.transaction_type == 'credit':
+#                             total_creditor += bs.amount
+#
+#             net_total = total_creditor - total_debtor
+#
+#             data.append(serialize_node(account, net_total))
+#         form_html = render_to_string('account_base/accounts.html', {
+#             'uniqueid': request.GET.get('uid', 'default')
+#         })
+#         return JsonResponse({'form_html': form_html, 'data': data})
+
 class AccountsView(generic.View):
     def get(self, request):
         def serialize_node(node, net_total):
@@ -200,33 +233,69 @@ class AccountsView(generic.View):
                 'li_attr': {'data-net-total': net_total},
             }
 
-        accounts = AccountsClass.objects.filter(user=request.user).prefetch_related(
-            Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user)))
-        data= []
+        # همه‌ی حساب‌ها رو با parent واکشی کن
+        accounts = AccountsClass.objects.filter(user=request.user).select_related('parent')
+        balances = BalanceSheet.objects.filter(user=request.user)
+        # همه‌ی تراکنش‌های کاربر رو یکجا بگیر
+        if request.GET.get('start-date'):
+            start_date = str(request.GET.get('start-date'))
+            balances = balances.filter(user=request.user, document__date_created__gte=start_date)
+        if request.GET.get('end-date'):
+            end_date = str(request.GET.get('end-date'))
+            balances = balances.filter(user=request.user,document__date_created__lte=end_date)
+
+        # جمع تراکنش‌ها بر اساس account_id
+        totals = balances.values('account_id', 'transaction_type').annotate(total=Sum('amount'))
+
+        # دیکشنری برای دسترسی سریع
+        totals_dict = {}
+        for row in totals:
+            acc_id = row['account_id']
+            ttype = row['transaction_type']
+            amount = row['total'] or 0
+            if acc_id not in totals_dict:
+                totals_dict[acc_id] = {'debt': 0, 'credit': 0}
+            totals_dict[acc_id][ttype] += amount
+
+        data = []
         for account in accounts:
+            # گرفتن descendantها (از treebeard یا mptt فرض می‌کنیم)
             descendants = account.get_descendants(include_self=True)
+
             total_debtor = 0
             total_creditor = 0
-            for descendant in descendants:
-                for bs in descendant.balance_sheets.all():
-                    if bs.amount:
-                        if bs.transaction_type == 'debt':
-                            total_debtor += bs.amount
-                        elif bs.transaction_type == 'credit':
-                            total_creditor += bs.amount
+            for desc in descendants:
+                if desc.id in totals_dict:
+                    total_debtor += totals_dict[desc.id]['debt']
+                    total_creditor += totals_dict[desc.id]['credit']
 
             net_total = total_creditor - total_debtor
-
             data.append(serialize_node(account, net_total))
+
         form_html = render_to_string('account_base/accounts.html', {
             'uniqueid': request.GET.get('uid', 'default')
         })
+
         return JsonResponse({'form_html': form_html, 'data': data})
+
 
 class AccountReportDetails(generic.View):
     def get(self, request, pk):
         parent_account = AccountsClass.objects.filter(pk=pk, user=request.user)
-        descendants = (parent_account.get_descendants(include_self=True).prefetch_related(Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user))))
+        descendants = (parent_account.get_descendants(include_self=True).prefetch_related(
+        Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user))))
+        created_at_from = request.GET.get('created_at_from')
+        created_at_to = request.GET.get('created_at_to')
+        if created_at_from and created_at_to:
+            descendants = (parent_account.get_descendants(include_self=True).prefetch_related(
+                Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user, document__date_created__gte=created_at_from, document__date_created__lte=created_at_to))))
+        elif created_at_from:
+            descendants = (parent_account.get_descendants(include_self=True).prefetch_related(
+                Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user, document__date_created__gte=created_at_from))))
+        elif created_at_to:
+            descendants = (parent_account.get_descendants(include_self=True).prefetch_related(
+                Prefetch('balance_sheets', queryset=BalanceSheet.objects.filter(user=request.user,
+                                                                                document__date_created__lte=created_at_to))))
         qs = []
         id_lists = []
         for descendant in descendants:
@@ -236,7 +305,8 @@ class AccountReportDetails(generic.View):
         qs = sorted(qs, key=lambda b: b.document.date_created, reverse=True)
         context = {
             'balance_lists':qs,
-            'id_lists': id_lists
+            'id_lists': id_lists,
+
         }
 
         return render(request, 'account_base/balance_list.html', context)
