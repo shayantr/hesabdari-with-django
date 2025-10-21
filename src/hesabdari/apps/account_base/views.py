02b67d979fe373833b03676e56c110a4d70305de
shayantr <61402605+shayantr@users.noreply.github.com>
2025-10-21 19:38:38 +0330

    add colors to cheque status on cheque-lists

--- src/hesabdari/apps/account_base/views.py
+++ src/hesabdari/apps/account_base/views.py
@@ -1070,10 +1070,11 @@ def filter_balance(request):
         people = {i.account.name for i in doc.items.all() if item.transaction_type != i.transaction_type}
         item.people = people
         balance_list.append(item)
+    balance_list = list(reversed(balance_list))
 
     paginator = Paginator(balance_list, 20)
     current_page = paginator.get_page(page)
-    html = render_to_string('partials/search_balance.html', {'balance_lists': reversed(current_page)})
+    html = render_to_string('partials/search_balance.html', {'balance_lists': current_page})
     return JsonResponse(
         {
             'html': html,
@@ -1083,6 +1084,74 @@ def filter_balance(request):
             'pre_total_debt': pre_total_debt,
             'has_next': current_page.has_next(),
             'next_page_number': current_page.next_page_number() if current_page.has_next() else None,})
+
+
+
+# def csv_balance(request):
+#     current_day = jdatetime.date.today()
+#     if request.GET.get('id_lists'):
+#         id_lists_json = request.GET.get('id_lists', '[]')
+#         id_lists = json.loads(id_lists_json)
+#         id_lists = [int(i) for i in id_lists]
+#         qs = BalanceSheet.objects.filter(
+#             Q(user=request.user.id, id__in=id_lists),
+#         ).select_related('document').prefetch_related('document__items__account')
+#     else:
+#         qs = BalanceSheet.objects.filter(
+#             Q(user=request.user.id)).select_related('document').prefetch_related('document__items__account')
+#
+#     # فیلترها
+#     account_id = request.GET.get('account_id')
+#     amount = request.GET.get('amount')
+#     balance_id = request.GET.get('balance_id')
+#     document_id = request.GET.get('document_id')
+#     created_at_from = request.GET.get('created_at_from')
+#     created_at_to = request.GET.get('created_at_to')
+#     description = request.GET.get('description')
+#     page = request.GET.get('page')
+#     debt_condition = {'transaction_type': 'debt'}
+#     credit_condition = {'transaction_type': 'credit'}
+#     if balance_id:
+#         qs = qs.filter(id=int(balance_id))
+#     if document_id:
+#         qs = qs.filter(document_id=document_id)
+#     if amount:
+#         qs = qs.filter(amount=amount)
+#     if description:
+#         qs = qs.filter(description__contains=description)
+#     if account_id:
+#         accounts = AccountsClass.objects.get(id=account_id)
+#         accounts = accounts.get_descendants(include_self=True)
+#         qs = qs.filter(account__in=accounts)
+#     pre_qs = qs
+#
+#     wb = openpyxl.Workbook()
+#     ws = wb.active
+#     ws.title = 'balance_sheet_csv'
+#     ws.append(['حساب', "مبلغ", "صاحب چک", "در وجه", "بانک", 'شماره چک', 'وضعیت چک', "توضیحات"])
+#     for item in qs:
+#         ws.append([
+#             item.cheque.maturity_date.strftime("%Y-%m-%d") if item.cheque.maturity_date else "",
+#             item.amount,
+#             item.cheque.name,
+#             item.cheque.in_cash,
+#             item.cheque.account.__str__(),
+#             item.cheque.Cheque_numbder,
+#             item.cheque.cheque_status,
+#             item.description or "",
+#         ])
+#     response = HttpResponse(
+#         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+#     )
+#     today = str(jdatetime.date.today())
+#     filename = f"{cheque_type}_cheques_{today}.xlsx"
+#     filename = urllib.parse.quote(filename)
+#     response['Content-Disposition'] = f"attachment; filename*=UTF-8''{filename}"
+#     wb.save(response)
+#     return response
+
+
+
 
 
 class ChangeStatusCheque(generic.View):
