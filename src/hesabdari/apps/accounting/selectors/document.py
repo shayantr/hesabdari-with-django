cc6d19b4bfffdaf4460560ddea5c5c84ab456d43
shayan <shayantr2@gmail.com>
2026-01-01 19:43:03 +0330

    git commit -m "Clean repository and fix gitignore"

--- src/hesabdari/apps/accounting/selectors/document.py
+++
@@ -1,8 +0,0 @@
-from hesabdari.apps.accounting.models import BalanceSheet
-
-
-def get_document_balances_for_update(document, user):
-    return BalanceSheet.objects.filter(document=document, user=user).select_related("account", "cheque__account").prefetch_related("cheque__balance_sheet")
-
-def get_document_balances_for_change_status(id, user):
-    return BalanceSheet.objects.filter(pk=id, user=user, is_active=True).select_related("cheque", "cheque__account")
--- src/hesabdari/apps/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from hesabdari.apps.home import views
-
-urlpatterns =[
-    path('', views.home, name='home'),
-    path('sidebar/', views.sidebar, name='sidebar'),
-]
